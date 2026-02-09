0ae9685c613ad831a5b7fac94d87e6d8a45d07a4
P.JH <pjh58910@gmail.com>
2026-02-09 09:44:10 +0900

    help

--- route/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const taskApi = require('./task.api'); // 1. task.api.js 불러오기
-
-// 2. /tasks 경로로 들어오면 taskApi로 넘겨줌
-router.use('/tasks', taskApi);
-
-module.exports = router; // 3. 이 통로를 밖으로 내보냄
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const taskApi = require("./task.api");
+
+router.use("/tasks", taskApi);
+module.exports = router;
